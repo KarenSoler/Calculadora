a2fd1edd2c4d524c21a647b6a912e6a50326e8be
KarenSoler <kjsoler19@misena.edu.co>
2020-09-11 16:28:01 -0500

    Solicitar dia

--- scripts.js
+++ scripts.js
@@ -1,27 +1,44 @@
-function pintarNumero(numero){
+swal("Hello World",);
+
+function pintarNumero(numero) {
     //document.getElementById('data').value=numero
     pintarDatos(numero)
 }
 
-function pintarOperador(operador){
+function pintarOperador(operador) {
     //document.getElementById('data').value=operador
     pintarDatos(` ${operador} `)
 }
 
-function pintarDatos(dato){
-    let valorInput=document.getElementById('data')
+function pintarDatos(dato) {
+    let valorInput = document.getElementById('data')
     //valorInput.value=valorInput.value + " " + dato
     valorInput.value = `${valorInput.value}${dato}`
 }
-function Limpiar(){
+function Limpiar() {
     document.getElementById('data').value = null
 }
-function mostrarResultado(){
+function mostrarResultado() {
     let valorInput = document.getElementById('data').value
-    let arreglo = valorInput.split (' ')
+    let arreglo = valorInput.split(' ')
     console.log(arreglo)
     let suma = parseInt(arreglo[0]) + parseInt(arreglo[2])
     document.getElementById('data').value = suma
+
+    switch (arreglo[1]) {
+        case '+':
+            console.log('suma')
+            let suma = parseInt(arreglo[0] + parseInt(arreglo[2]))
+            document.getElementById('data').value = suma
+            break;
+        case '-':
+            console.log('resta')
+            let resta = parseInt(arreglo[0] - parseInt(arreglo[2]))
+            document.getElementById('data').value = resta
+            break;
+        default:
+            alert('No es + Ni -')
+    }
 }
 /*if (arreglo[1] =='+'){
     let suma =parseInt(arreglo[0]+parseInt(arreglo[2]))
@@ -38,31 +55,34 @@ else {
     let dividir=parseInt(arreglo[0]/parseInt(arreglo[2]))
     document.getElementById('data').value=dividir
 }*/
-switch (arreglo[1]){
-    case '+':
-        console.log('suma')
-        let suma = parseInt(arreglo[0]+parseInt(arreglo[2]))
-        document.getElementById('data').value=suma
-        break;
-    case '-':
-        console.log('resta')
-        let resta = parseInt(arreglo[0]-parseInt(arreglo[2]))
-        document.getElementById('data').value=resta
-        break;
-        default:
-            alert('No es + Ni -')    
-    }
 
-    function preguntarDia(){
-        prompt("¿Qué día quieres saber?")
-        let dia=document.getElementById("dia")
+function preguntarDia() {
+    let dia = prompt("¿Qué día quieres saber?")
+
+    switch (dia) {
+        case 'Domingo':
+            alert("Has Digitado Domingo")
+            break;
+        case 'Lunes':
+            alert("Has Digitado Lunes")
+            break;
+        case 'Martes':
+            alert("Has Digitado Martes")
+            break;
+        case 'Miercoles':
+            alert("Has Digitado Miercoles")
+            break;
+        case 'Jueves':
+            alert("Has Digitado Jueves")
+            break;
+        case 'Viernes':
+            alert("Has Digitado Viernes")
+            break;
+        case 'Sabado':
+            alert("Has Digitado Sabado")
+            break;
+        default:
+            alert("Has Digitado"+prompt)
     }
-   switch (dia){
-   case 'Lunes':
-       prompt("Hoy es LUNES")
-       let dia=document.getElementById("dia") 
-       break;
-    case  'Martes':
-        prompt("Hoy es MARTES")   
-        let dia=document.getElementById 
 }
+
